84a2fa1ed710bba96931db0968fe904e85bd7291
kadaokaduan <manhongpeng2711@163.com>
2018-08-24 13:10:00 +0800

    test

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 30
+
 int f(char * s) {
 	char buf[20]; //buf not set to use SIZE macro
 
 	strncpy(buf, s, SIZE); //wrong: copy may exceed size of buf
 
 	for (int i = 0; i < SIZE; i++) { //wrong: upper limit that is higher than array size
-		cout << array[i];
+		printf("%c", array[i]);
 	}
 }
 /*
