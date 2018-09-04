ea9984836c56747c42f7648ebb08dc5647b60ea3
kadaokaduan <manhongpeng2711@163.com>
2018-09-04 10:21:27 +0800

    test

--- main.c
+++ main.c
@@ -4,22 +4,17 @@
 
 #define SIZE 30
 
-int f(char * s) {
-	char buf[20]; //buf not set to use SIZE macro
-
-	strncpy(buf, s, SIZE); //wrong: copy may exceed size of buf
-
-	for (int i = 0; i < SIZE; i++) { //wrong: upper limit that is higher than array size
-		printf("%c", buf[i]);
-	}
+int foo(char **s) {
+	char string[16]; //buf not set to use SIZE macro
+	strcpy(string, *s); //wrong: copy may exceed size of buf
 }
 /*
 */
 void main(int argc, char *argv[]) {
 system("color a");
  if(argc < 2) {
- printf("Usage: %s <password>\n", argv[0]);
+ printf("Usage: %s <string>\n", argv[0]);
  exit(0);
  }
- int a = f(argv[1]);
+ int a = foo(&argv[1]);
 }
